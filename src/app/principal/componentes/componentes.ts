8af5886fdde18f461f17873581276abdb5d25695
Stiven <johnstiven40@gmail.com>
2025-08-25 20:38:33 -0500

    avance

---
+++ src/app/principal/componentes/componentes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componentes',
+  imports: [],
+  templateUrl: './componentes.html',
+  styleUrl: './componentes.css'
+})
+export class Componentes {
+
+}
---
+++ src/app/principal/estructura/estructura.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estructura',
+  imports: [],
+  templateUrl: './estructura.html',
+  styleUrl: './estructura.css'
+})
+export class Estructura {
+
+}
