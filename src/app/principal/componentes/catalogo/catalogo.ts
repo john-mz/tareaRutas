1ef6deffb3273609611807b49ca642c4cd816d49
Stiven <johnstiven40@gmail.com>
2025-08-29 12:43:08 -0500

    crud hecho falta estilo

--- src/app/principal/componentes/catalogo/catalogo.ts
+++ src/app/principal/componentes/catalogo/catalogo.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CatalogoService } from './catalogo.service';
+import { Agregar } from './agregar/agregar';
 
 @Component({
   selector: 'app-catalogo',
-  imports: [],
+  imports: [Agregar],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css'
 })
 export class Catalogo {
+  IsAgregar = false;
 
+  onStartAdding(){
+    this.IsAgregar = true;
+  }
+
+  onCloseAdding(){
+    this.IsAgregar = false;
+  }
+
+  constructor(private CatalogoService: CatalogoService){}
+
+  eliminar(nombre: string){
+    this.CatalogoService.eliminar(nombre);
+  }
+
+  editar(){
+    
+    this.CatalogoService.editar();
+  }
+  get consultar(){
+    return this.CatalogoService.consultar();
+  }
 }
